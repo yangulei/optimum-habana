046691beda8927204766e858094e8f572b52184e
Youlei Yang <youlei.yang@intel.com>
2024-01-16 03:49:46 -0800

    save tmp work

--- examples/text-generation/run_qwen.sh
+++
@@ -1,8 +0,0 @@
-python run_generation.py \
-    --model_name_or_path /workspace/models/qwen-7b \
-    --trust_remote_code \
-    --use_hpu_graphs \
-    --bucket_size 512 \
-    --use_kv_cache \
-    --max_new_tokens 100 \
-    --prompt "Here is my prompt"
